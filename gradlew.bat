069c97e1e721975f589329741c3eb4a01d01d34e
ghaithdallaali <ghaith.hadi@gmail.com>
2016-09-28 17:51:33 -0400

    added gradle-wrapper.jar to gradle build dir, modified gitignore to exclude .jar

--- gradlew.bat
+++ gradlew.bat
@@ -71,6 +71,8 @@ set CMD_LINE_ARGS=%$
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+echo %CLASSPATH%
+
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
